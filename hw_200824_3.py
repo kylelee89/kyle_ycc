757ccc410b0b2f9d91a6aebd7dfaaa6762c9db04
kylelee89 <leesg0714@gmail.com>
2020-08-24 18:41:46 +0900

    math

---
+++ hw_200824_3.py
@@ -0,0 +1,27 @@
+# https://www.hackerrank.com/challenges/python-mod-divmod/problem, mod divmod
+
+# enter your code here. read input from stdin. print output to stdout
+a = int(input())
+b = int(input())
+print(a // b)
+print(a % b)
+print((a // b, a % b)
+
+# https://www.hackerrank.com/challenges/python-power-mod-power/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+print(a ** b)
+print(pow(a, b, m))
+
+#https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+  # Enter your code here. Read input from STDIN. Print output to STDOU
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(a ** b + c ** d)
+
