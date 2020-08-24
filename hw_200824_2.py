4ffd0e01bea440cb9098942d264325cffcac9cb1
kylelee89 <leesg0714@gmail.com>
2020-08-24 18:02:01 +0900

    completed

---
+++ hw_200824_2.py
@@ -0,0 +1,20 @@
+# 삼성물산 PER/PBR 구하기
+
+net_profit_st = 104789
+bookvalue_st = 2669209
+marketvalue_st = 21865788
+
+per_st = marketvalue_st / net_profit_st
+pbr_st = bookvalue_st / marketvalue_st
+
+# 에이텍 PER/PBR 구하기
+net_profit_at = 6421
+bookvalue_at = 75506
+marketvalue_at = 314467
+
+per_at = marketvalue_at / net_profit_at
+pbr_st = bookvalue_at / marketvalue_at
+
+
+print(f"삼성물산 PER은 {per_st}, PBR은 {pbr_st}, 에이텍의 PER은 {per_st}, PBR은 {pbr_st}")
+
